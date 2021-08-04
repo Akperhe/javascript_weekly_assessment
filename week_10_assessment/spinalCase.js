6fc83f09f4a8f8a5463999a6ec575ade2d396505
Akperhe <theakperheovo@gmail.com>
2021-08-04 14:18:15 -0700

    Spinal Tap Case

--- week_10_assessment/spinalCase.js
+++ week_10_assessment/spinalCase.js
@@ -1,7 +1,7 @@
 function spinalCase(str) {
     str = str.split(/(?=[A-Z])/).join(" ");
 
-    var regex = /_+|\s+|-+|;+/g; // you can add more matches you which to replace, to this regular expression
+    var regex = /_+|\s+|;+|-+/g; // you can add more matches before the last "-". You which to replace, to this regular expression
 
     str = str.replace(regex, '-');
     return str.toLowerCase();
